5a49e95e8a82e086b513b80c604ff9e533108fb2
Hoang Nguyen <nnhuyhoang97@gmail.com>
2023-08-04 13:36:07 +0700

    add workflow

--- services/one/main.go
+++ services/one/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/one/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, hello.Greet("Tim"))
+		return c.String(http.StatusOK, hello.Greet("World"))
 	})
 	_ = e.Start(":8080")
 }
